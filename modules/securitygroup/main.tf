cbf7a4c7d1d79773c7ea02ae86dd72d212f90db7
Sai Srinivas Idimadakala <saisrinivas.idimadakala@gmail.com>
2025-07-01 11:57:08 +0530

    added the egress rule

--- modules/securitygroup/main.tf
+++ modules/securitygroup/main.tf
@@ -3,30 +3,15 @@ resource "aws_security_group" "main" {
   description = var.sg_description
   vpc_id      = var.vpc_id
 
-/* // Inbound rule: allow HTTP traffic
-  ingress {
-    from_port   = 80
-    to_port     = 80
-    protocol    = "tcp"
-    cidr_blocks = ["0.0.0.0/0"]
-  }
-
-  ingress {
-    from_port   = 22
-    to_port     = 22
-    protocol    = "tcp"
-    cidr_blocks = ["0.0.0.0/0"]
-  }
-
-  // Outbound rule: allow all traffic
+// Outbound rule: allow all traffic
   egress {
     from_port   = 0
     to_port     = 0
     protocol    = "-1"
     cidr_blocks = ["0.0.0.0/0"]
-  } */
+  }
 
-
+  
   tags = merge(
     var.sg_tags,
     local.common_tags,{
