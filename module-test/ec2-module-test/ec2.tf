5523c20bcefdcb8bf3963a6640e80f429da32dec
Sai Srinivas Idimadakala <saisrinivas.idimadakala@gmail.com>
2025-07-16 01:41:57 +0530

    added ec2 module

--- module-test/ec2-module-test/ec2.tf
+++ module-test/ec2-module-test/ec2.tf
@@ -1,15 +1,14 @@
 module "ec2" {
-    source = "git::https://github.com/Idimadakala/terraform-aws-resources.git//modules/ec2?ref=develop"
+    source = "../../modules/ec2"
     ami_id = local.ami_id
-    #sg_ids = var.sg_ids
-    sg_ids = local.sg_id
+    sg_ids = [local.sg_id]
     instance_type = var.instance_type
     tags = var.tags
     #vpc_security_group_ids = [local.sg_id]
 }
 
 module "securitygroup" {
-    source = "git::https://github.com/Idimadakala/terrafrom-aws-resources.git//modules/securitygroup?ref=develop"
+    source = "../../modules/securitygroup"
     sg_name = var.sg_name
     sg_description = var.sg_description
     vpc_id = local.vpc_id
