907ce9034a00d834b49afe482495947c9f456e4f
Sai Srinivas Idimadakala <saisrinivas.idimadakala@gmail.com>
2025-07-16 00:48:34 +0530

    added ec2 module

---
+++ module-test/ec2-module-test/ec2.tf
@@ -0,0 +1,16 @@
+module "ec2" {
+    source = "../modules/ec2"
+    ami_id = local.ami_id
+    sg_ids = var.sg_ids
+    instance_type = var.instance_type
+    tags = var.tags
+}
+
+module "ec2_security_group" {
+    source = "../modules/securitygroup"
+    sg_name = var.sg_name
+    sg_description = var.sg_description
+    vpc_id = local.vpc_id
+    project = var.project
+    environment = var.environment
+}
---
+++ module-test/ec2-module-test/locals.tf
@@ -0,0 +1,4 @@
+locals {
+  ami_id = data.aws_ami.joindevsops.id
+  vpc_id = data.aws_vpc.default.id
+}
